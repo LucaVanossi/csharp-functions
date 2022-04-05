a26385cbc76692eb81b0569ba9e4986d3eedf29a
LucaVanossi <vanossi.luca13@gmail.com>
2022-04-05 15:30:53 +0200

    1 e 2 funzione ok

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+void StampaArrayInteri(int[] array)
+{
+    int ultimoIndiceArray = array.Length - 1;
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == ultimoIndiceArray)
+        {
+            Console.Write(array[i]);
+        }
+        else
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+
+
+int Quadrato(int numero)
+{
+    int risultato = (numero * numero);
+    return risultato;
+}
+
+/*int NumeroFinale = Quadrato(5);
+Console.WriteLine(NumeroFinale);*/   
+
+
+
